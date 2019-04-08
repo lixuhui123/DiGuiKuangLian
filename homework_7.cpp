cc341c9830598ec20cdb8b1a290abcbdabf3e01f
lixuhui-123 <2871726754@qq.com>
2019-04-08 22:29:22 +0800

    递归求一个斐波那契数列

---
+++ homework_7.cpp
@@ -0,0 +1,23 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//int JieCheng(int f)
+//{
+//	int n = 0;
+//	if (f < 0)
+//		printf("您的输入非法");
+//	else if (0 == f || 1 == f)
+//		n = 1;
+//	else n = JieCheng(f - 1)*f;
+//	return n;
+//}
+//int main()
+//{
+//	int key = 0;
+//	printf("请输入要计算阶乘的数\n");
+//	scanf("%d", &key);
+//	int zhi=JieCheng(key);
+//	printf("%d的阶乘是%d\n", key,zhi);
+//	system("pause");
+//	return 0;
+//}
