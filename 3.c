2f3314778de86acb48846b22a9289c6b0784d474
lixuhui-123 <2871726754@qq.com>
2019-04-10 23:20:50 +0800

    为了绿色

---
+++ 3.c
@@ -0,0 +1,65 @@
+//
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int ChengFang(int k)
+//{
+//	/*int n = 0;阶乘
+//	if (f < 0)
+//		printf("您的输入非法");
+//	else if (0 == f || 1 == f)
+//		n = 1;
+//	else n = JieCheng(f - 1)*f;
+//	return n;*/ 
+//	//int k = 0;
+//	int f = 0;
+//	printf("请输入要计算阶乘的数\n");
+//	    
+//	scanf("%d", &f);
+//	    k = f;
+//	if (f == 1)
+//		k = f;
+//	else 
+//		k = ChengFang(k-1) * k;//chengfang
+//	return k;
+//}
+//int main()
+//{
+//	int key = 0;
+//	int fang = 0;
+//	printf("请输入要计算的数的乘方\n");
+//	scanf("%d",&fang);
+//	
+//	int zhi=ChengFang(fang);
+//	printf("乘方是%d\n",zhi);
+//	system("pause");
+//	return 0;
+//}
+//#include <stdio.h>
+//#include <stdlib.h>
+//int Factor(int n, int k)
+//{
+//	if (k == 1)
+//	{
+//		return n;
+//	}
+//	else if (k == 0)
+//	{
+//		return 1;
+//	}
+//  else
+//	return n * Factor(n, k - 1);
+//}
+//
+//
+//int main()
+//{
+//	int n;
+//	int k;
+//	printf("请输入要计算的数和它的阶乘\n");
+//	scanf_s("%d %d", &n, &k);
+//	printf("%d\n", Factor(n, k));
+//	system("pause");
+//	return 0;
+//}
+
